2e6f79458de7a7ebef9b80b2c929e0cd62a3b93d
dongshu2013 <dongs2011@gmail.com>
2023-12-13 11:55:15 -0800

    publish new contract pkg and some nits

--- deploy/03_deploy_verax.ts
+++ deploy/03_deploy_verax.ts
@@ -5,30 +5,30 @@ import * as fs from "fs";
 import { ethers } from "ethers";
 
 function getSigner(hre: HardhatRuntimeEnvironment) {
-    if (hre.network.name === "linea" || hre.network.name === "linea_test") {
-        const env = JSON.parse(
-            fs.readFileSync(process.env.LOCAL_KEYS_PATH!, "utf8"));
-        return new hre.ethers.Wallet(env.openid3, hre.ethers.provider);
-    } else {
-        throw new Error("Unsupported network");
-    }
+    const env = JSON.parse(
+        fs.readFileSync(process.env.LOCAL_KEYS_PATH!, "utf8"));
+    return new hre.ethers.Wallet(env.openid3, hre.ethers.provider);
 }
 
 const func: DeployFunction = async function(hre: HardhatRuntimeEnvironment) {
-    const deployer = getSigner(hre);
-    const args = ethers.AbiCoder.defaultAbiCoder().encode(
-        ["address"],
-        ["0xf3b4e49Fd77A959B704f6a045eeA92bd55b3b571"]
-    );
-    const feeData = await hre.ethers.provider.getFeeData();
-    await deterministicDeploy(
-        hre,
-        "OpenId3TeeModule",
-        genBytecode(await hre.artifacts.readArtifact("OpenId3TeeModule"), args),
-        hre.ethers.ZeroHash,
-        deployer,
-        feeData.gasPrice!,
-    );
+    if (hre.network.name === "linea" || hre.network.name === "linea_test") {
+        const deployer = getSigner(hre);
+        const args = ethers.AbiCoder.defaultAbiCoder().encode(
+            ["address"],
+            ["0xf3b4e49Fd77A959B704f6a045eeA92bd55b3b571"]
+        );
+        const feeData = await hre.ethers.provider.getFeeData();
+        await deterministicDeploy(
+            hre,
+            "OpenId3TeeModule",
+            genBytecode(await hre.artifacts.readArtifact("OpenId3TeeModule"), args),
+            hre.ethers.ZeroHash,
+            deployer,
+            feeData.gasPrice!,
+        );
+    } else {
+        console.log("Skipping verax deploy on unsupported network");
+    }
 }
 
 export default func;
