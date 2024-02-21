e43207afd75ccf341d08b76712140e0ac4c1f36a
dongshu2013 <dongs2011@gmail.com>
2024-02-20 23:47:11 -0800

    support customized attestation

--- contracts/attestation/consumer/SocialVoting.sol
+++ contracts/attestation/consumer/SocialVoting.sol
@@ -9,17 +9,15 @@ contract SocialVoting is IAttestationConsumer {
 
     event NewVote(uint256 from, address to, uint256 day);
 
-    mapping(uint256 => mapping(uint256 => bool)) _voted;
-    mapping(address => mapping(uint256 => uint256)) _history;
+    mapping(address => mapping(uint256 => mapping(uint256 => bool))) _voted;
+    mapping(address => mapping(address => mapping(uint256 => uint256))) _history;
     address immutable allowed;
 
     constructor(address _allowed) {
         allowed = _allowed;
     }
 
-    function onNewAttestation(
-        AttestationEvent calldata e
-    ) external override {
+    function onNewAttestation(AttestationEvent calldata e) external override {
         if (msg.sender != allowed) {
             revert UnauthorizedCaller();
         }
@@ -28,23 +26,28 @@ contract SocialVoting is IAttestationConsumer {
             return;
         }
         address to = address(bytes20(e.data));
-        _history[to][day] = _history[to][day] + 1;
-        _voted[e.from][day] = true;
+        _history[msg.from][to][day] = _history[msg.from][to][day] + 1;
+        _voted[msg.from][e.from][day] = true;
         emit NewVote(e.from, to, day);
     }
 
-    function isVoted(uint256 from, uint256 day) external view returns (bool) {
-        return _voted[from][day];
+    function isVoted(
+        address attestation,
+        uint256 from,
+        uint256 day
+    ) external view returns (bool) {
+        return _voted[attestation][from][day];
     }
 
     function totalVoted(
+        address attestation,
         uint256 from,
         uint256 start,
         uint256 numOfDays
     ) external view returns (uint256) {
         uint256 total = 0;
         for (uint i = start; i < start + numOfDays; i++) {
-            if (_voted[from][i]) {
+            if (_voted[attestation][from][i]) {
                 total = total + 1;
             }
         }
@@ -52,13 +55,14 @@ contract SocialVoting is IAttestationConsumer {
     }
 
     function totalConsecutiveVoted(
+        address attestation,
         uint256 from,
         uint256 start,
         uint256 numOfDays
     ) external view returns (uint256) {
         uint256 total = 0;
         for (uint i = start; i < start + numOfDays; i++) {
-            if (_voted[from][i]) {
+            if (_voted[attestation][from][i]) {
                 total = total + 1;
             } else if (total > 0) {
                 total = total - 1;
@@ -68,13 +72,14 @@ contract SocialVoting is IAttestationConsumer {
     }
 
     function totalVotes(
+        address attestation,
         address to,
         uint256 start,
         uint256 numOfDays
     ) external view returns (uint256[] memory) {
         uint256[] memory votes = new uint256[](numOfDays);
         for (uint256 i = start; start < start + numOfDays; start++) {
-            votes[i - start] = _history[to][i];
+            votes[i - start] = _history[attestation][to][i];
         }
         return votes;
     }
